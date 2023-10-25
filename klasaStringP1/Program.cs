a84b066107a9cdf1570fe96565214051587e1ff0
ivanoVucak <145533812+ivanoVucak@users.noreply.github.com>
2023-10-25 08:14:19 +0200

    Add files via upload

---
+++ klasaStringP1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasaStringP1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string tekst = "Hrvatska ima more";
+            string tekst2 = "Italija je prvak svijeta";
+
+            Console.Write(tekst.Substring(0, 8));
+            Console.WriteLine(tekst2.Substring(7, tekst.Length));
+
+            Console.ReadKey();
+        }
+    }
+}
